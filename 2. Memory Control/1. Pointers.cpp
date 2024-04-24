d6998a4d5d74f5476fccba9da3150eeef3d34eb8
adityarahalkar925@gmail.com <adityarahalkar925@gmail.com>
2024-04-24 18:48:52 +0530

    minor change

--- 2. Memory Control/1. Pointers.cpp
+++ 2. Memory Control/1. Pointers.cpp
@@ -11,7 +11,7 @@ int Pointers(){
 	std::cout << "Value of pointer count is: " << p_count << std::endl;
 
 	
-	*p_count = *p_count + 1;		// This statement updates value of count on same memory location.(Dereferencing)
+	*p_count = *p_count + 1;		// This statement updates value of count on the same memory location.(Dereferencing)
 
 	std::cout << "Updated Value of count is: " << count << std::endl;
 	std::cout << "Value of pointer count is: " << p_count << std::endl;
